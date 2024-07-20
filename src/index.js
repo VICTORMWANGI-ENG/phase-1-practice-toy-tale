db55dc6a746849ead450bd0ae02ed6b75ed0aefe
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-07-21 01:26:51 +0300

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
+/** @format */
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+	const addBtn = document.querySelector("#new-toy-btn");
+	const toyFormContainer = document.querySelector(".container");
+	addBtn.addEventListener("click", () => {
+		// hide & seek with the form
+		addToy = !addToy;
+		if (addToy) {
+			toyFormContainer.style.display = "block";
+		} else {
+			toyFormContainer.style.display = "none";
+		}
+	});
 });
+fetch("http://localhost:3000/toys")
+	.then((res) => res.json())
+	.then((data) => {
+		const toys = data;
+		displayToy(toys);
+	});
+
+function displayToy(toys) {
+	toys.forEach((toy) => {
+		let card = document.createElement("div");
+		card.className = "card";
+		let name = document.createElement("h2");
+		let image = document.createElement("img");
+		image.className = "toy-avatar";
+		let likes = document.createElement("p");
+		let button = document.createElement("button");
+		button.className = "likeBtn";
+		name.textContent = toy.name;
+		image.src = toy.image;
+		likes.textContent = `Likes: ${toy.likes}`;
+		button.textContent = "Like";
+		//
+		let toyContainer = document.querySelector("#toy-collection");
+		//
+		card.appendChild(name);
+		card.appendChild(image);
+		card.appendChild(likes);
+		card.appendChild(button);
+		toyContainer.appendChild(card);
+		////
+
+		let likeBtn = document.querySelector(".likeBtn");
+		likeBtn.addEventListener("click", () => {
+			likes.textContent = `Likes: ${(toy.likes += 1)}`;
+		});
+  });
+ 
+}
